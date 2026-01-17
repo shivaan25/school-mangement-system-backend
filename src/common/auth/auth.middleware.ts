3a5e6e93fd85bdefa79bee609f917b88cfdbb8a4
Shivam Singla <singlashivam1000@gmail.com>
2026-01-17 15:07:07 +0530

    adding authmiddleware for student login

---
+++ src/common/auth/auth.middleware.ts
@@ -0,0 +1,27 @@
+import { Request, Response, NextFunction } from "express";
+import jwt from "jsonwebtoken";
+import { env } from "../../config/env";
+
+export const authMiddleware = (
+  req: Request & { user: any },
+  res: Response,
+  next: NextFunction,
+) => {
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const tokenDecode = jwt.verify(token, env.JWT_SECRET);
+    req.user = tokenDecode;
+    next();
+  } catch {
+    return res.status(401).json({
+      message: "Token Expired or invalid",
+    });
+  }
+};
