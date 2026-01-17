6fc4d28b0fc38f010787c4bff7d86e5539edf3ce
Shivam Singla <singlashivam1000@gmail.com>
2026-01-17 14:41:54 +0530

    creating Auth Module part-2

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,44 @@
+import * as authRepo from "./auth.repository";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import { env } from "../../config/env";
+
+export const register = async (data: any) => {
+  const existingUser = await authRepo.findUserByEmail(data.email);
+  if (existingUser) {
+    throw new Error("User ALready Exists");
+  }
+
+  const hashedPassword = await bcrypt.hash(data.password, 10);
+
+  return authRepo.createUser({
+    name: data.name,
+    email: data.email,
+    password: hashedPassword,
+    role: data.role || "student",
+  });
+};
+
+export const login = async (email: string, password: string) => {
+  const user = await authRepo.findUserByEmail(email);
+  if (!user) {
+    throw new Error("Please SignUP to register");
+  }
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if (!isMatch) {
+    throw new Error("Password is Incorrect");
+  }
+
+  const token = jwt.sign(
+    {
+      userId: user._id,
+      role: user.role,
+    },
+    env.JWT_SECRET,
+    {
+      expiresIn: "1d",
+    },
+  );
+  return { token };
+};
