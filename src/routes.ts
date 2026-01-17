253f16e06f8921ce4e2824349c7e0868fd6c44df
Shivam Singla <singlashivam1000@gmail.com>
2026-01-17 14:53:42 +0530

    Adding Auth Routes in routes.ts

--- src/routes.ts
+++ src/routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import studentRoutes from "./modules/students/student.routes";
 import schoolRoutes from "./modules/schools/school.routes";
+import authRoutes from "./modules/auth/auth.routes";
 
 const router = Router();
-
+router.use("/auth", authRoutes);
 router.use("/students", studentRoutes);
 router.use("/school", schoolRoutes);
 
